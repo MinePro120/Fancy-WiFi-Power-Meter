0fe985d8af2b5d6b71c2c38cc4903269beccf0fa
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-06-17 19:34:21 +0300

    Optimized printing of buffers
    -Now "logo" isn't printed inside the main loop anymore

--- wpower.c
+++ wpower.c
@@ -12,17 +12,6 @@ void handleSignal ()
 }
 
 
-void printLogo (char *buf)
-{
-  char tempStr[500];
-  strcpy (tempStr, "\e[1;34m\n __      ___     ___ _   ___                      __  __     _\n");
-  strcat (tempStr, " \\ \\    / (_)___| __(_) | _ \\_____ __ _____ _ _  |  \\/  |___| |_ ___ _ _\n");
-  strcat (tempStr, "  \\ \\/\\/ /| |___| _|| | |  _/ _ \\ V  V / -_) '_| | |\\/| / -_)  _/ -_) '_|\n");
-  strcat (tempStr, "   \\_/\\_/ |_|   |_| |_| |_| \\___/\\_/\\_/\\___|_|   |_|  |_\\___|\\__\\___|_|\n\n\e[0m");
-  strcat (buf, tempStr);
-}
-
-
 void printPower (int power, char *buf)
 {
   char tempStr[200] = "\e[1;32m";
@@ -124,20 +113,24 @@ int main (int argc, char **argv)
       fprintf (stderr, "\e[1;31mUsage : wpower [WIRELESS INTERFACE]\n\e[0m");
       exit (1);
     }
-  checkNic (argv[1]);
+  const char logo[] = "\e[1;34m\n __      ___     ___ _   ___                      __  __     _\n"
+  " \\ \\    / (_)___| __(_) | _ \\_____ __ _____ _ _  |  \\/  |___| |_ ___ _ _\n"
+  "  \\ \\/\\/ /| |___| _|| | |  _/ _ \\ V  V / -_) '_| | |\\/| / -_)  _/ -_) '_|\n"
+  "   \\_/\\_/ |_|   |_| |_| |_| \\___/\\_/\\_/\\___|_|   |_|  |_\\___|\\__\\___|_|\n\n\e[0m";
   while (1)
     {
-      char buf[500] = "";    // Output buffer
-      printLogo (buf);
+      char outBuf[600];    // Output buffer
+      char infoBuf[300];    // Info buffer
       char tempStr[50] = "";
       sprintf (tempStr, "\e[1;33m Interface : %s\n\e[0m", argv[1]);
-      strcat (buf, tempStr);
-      printEssid (argv[1], buf);
-      printRssi (argv[1], buf);
+      strcpy (infoBuf, tempStr);    // Store interface to buffer
+      printEssid (argv[1], infoBuf);
+      printRssi (argv[1], infoBuf);
       checkNic (argv[1]);    // Check if NIC is still up
       printf ("\033c");    // Clear screen
-      printf (buf);    // Print everything
+      strcpy (outBuf, logo);
+      printf (strcat (outBuf, infoBuf));    // Print everything
       sleep (1);    // Refresh delay
     }
-  exit (0);
+  return 0;
 }
