d4a26af656fccebd232b8b5f6b10bcabe1d15b6d
MinePro120 <37949795+MinePro120@users.noreply.github.com>
2020-06-18 22:50:15 +0300

    Major improvements
    -Added grey bars
    -Made colours brighter
    -Added average power
    -Seperated power from rssi
    -Improved rssi scale
    -Added support for 3-digit (and more) power values

--- wpower.c
+++ wpower.c
@@ -4,57 +4,13 @@
 #include <string.h>
 #include <signal.h>
 
+unsigned long long int counter = 1;
+
 
 void handleSignal ()
 {
-  printf ("\033c");
+  system ("clear");
   exit (0);
-}
-
-
-void printPower (int power, char *buf)
-{
-  char tempStr[200] = "\e[1;32m";
-  if (power >= -20)
-    {
-      strcat (tempStr, "                         __\n");
-      strcat (tempStr, "                      __|  |\n");
-      strcat (tempStr, " ,--..Y            __|  |  |\n");
-      strcat (tempStr, " \\   /`.        __|  |  |  |\n");
-      strcat (tempStr, "  \\.    \\    __|  |  |  |  |\n");
-      strcat (tempStr, "   \"\"\"--'   |  |  |  |  |  |");
-    }
-  else if (power >= -40)
-    {
-      strcat (tempStr, "                      __\n");
-      strcat (tempStr, " ,--..Y            __|  |\n");
-      strcat (tempStr, " \\   /`.        __|  |  |\n");
-      strcat (tempStr, "  \\.    \\    __|  |  |  |\n");
-      strcat (tempStr, "   \"\"\"--'   |  |  |  |  |");
-    }
-  else if (power >= -60)
-    {
-      strcat (tempStr, " ,--..Y            __\n");
-      strcat (tempStr, " \\   /`.        __|  |\n");
-      strcat (tempStr, "  \\.    \\    __|  |  |\n");
-      strcat (tempStr, "   \"\"\"--'   |  |  |  |");
-    }
-  else if (power >= -80)
-    {
-      strcat (tempStr, " ,--..Y\n");
-      strcat (tempStr, " \\   /`.        __\n");
-      strcat (tempStr, "  \\.    \\    __|  |\n");
-      strcat (tempStr, "   \"\"\"--'   |  |  |");
-    }
-  else
-    {
-      strcat (tempStr, " ,--..Y\n");
-      strcat (tempStr, " \\   /`.\n");
-      strcat (tempStr, "  \\.    \\    __\n");
-      strcat (tempStr, "   \"\"\"--'   |  |");
-    }
-  strcat (tempStr, "\e[0m\n");
-  strcat (buf, tempStr);
 }
 
 
@@ -65,7 +21,8 @@ void checkNic (char *nic)
   strcat (str, " > /dev/null");
   if (system (str) != 0)
     {
-      fprintf (stderr, "\e[0m\e[1;31mNo such wireless interface\n\e[0m");
+      system ("clear");
+      fprintf (stderr, "\e[0m\e[1;91mNo such wireless interface/Disconnected\n\e[0m");
       exit (1);
     }
 }
@@ -75,14 +32,14 @@ void printEssid (char *nic, char *buf)
 {
   char str[100] = "iwgetid | grep -w ";
   strcat (str, nic);
-  strcat (str, " | egrep -o '\"(.*?)\"'");
+  strcat (str, " | grep -E -o '\"(.*?)\"'");
   FILE *fp;
   fp = popen (str, "r");
   char essid[34];
   fgets (essid, sizeof (essid), fp);
   pclose (fp);
   char tempStr[50];
-  sprintf (tempStr, "\e[1;33m ESSID : %s\e[0m", essid);
+  sprintf (tempStr, "\e[1;93m ESSID : %s\e[0m", essid);
   strcat (buf, tempStr);
 }
 
@@ -91,16 +48,68 @@ void printRssi (char *nic, char *buf)
 {
   char str[100] = "cat /proc/net/wireless | grep -w ";
   strcat (str, nic);
-  strcat (str, " | cut -d ' ' -f 7 | cut -z -c 1-3");
+  strcat (str, " | cut -z -d '.' -f 2");
   FILE *fp;
   fp = popen (str, "r");
-  char rssi[4];
-  fgets (rssi, sizeof (rssi), fp);
+  char powerStr[10];
+  fgets (powerStr, sizeof (powerStr), fp);
   pclose (fp);
-  char tempStr[50];
-  sprintf (tempStr, "\e[1;33m RSSI : %s dBm\n\n\e[0m", rssi);
+  char tempStr[400];
+  int power = atoi (powerStr);
+  static unsigned long long int sum = 0;
+  sum += -power;
+  float avg = (float) sum / counter;
+  sprintf (tempStr, "\e[1;93m Power : %d dBm (-%1.1f avg.)\n\n\e[0m", power, avg);
   strcat (buf, tempStr);
-  printPower (atoi (rssi), buf);
+  //printPower (rssi, buf);
+  strcpy (tempStr, "");
+  if (power >= -30)    // Print ascii art
+    {
+      strcat (tempStr, "\e[1;92m                         __\n");
+      strcat (tempStr, "                      __|  |\n");
+      strcat (tempStr, " ,--..Y            __|  |  |\n");
+      strcat (tempStr, " \\   /`.        __|  |  |  |\n");
+      strcat (tempStr, "  \\.    \\    __|  |  |  |  |\n");
+      strcat (tempStr, "   \"\"\"--'   |  |  |  |  |  |");
+    }
+  else if (power >= -65)
+    {
+      strcat (tempStr, "\e[1;90m                         __\n");
+      strcat (tempStr, "\e[1;92m                      __\e[1;90m|  |\n");
+      strcat (tempStr, "\e[1;92m ,--..Y            __|  |\e[1;90m  |\n");
+      strcat (tempStr, "\e[1;92m \\   /`.        __|  |  |\e[1;90m  |\n");
+      strcat (tempStr, "\e[1;92m  \\.    \\    __|  |  |  |\e[1;90m  |\n");
+      strcat (tempStr, "\e[1;92m   \"\"\"--'   |  |  |  |  |\e[1;90m  |");
+    }
+  else if (power >= -70)
+    {
+      strcat (tempStr, "\e[1;90m                         __\n");
+      strcat (tempStr, "\e[1;90m                      __|  |\n");
+      strcat (tempStr, "\e[1;92m ,--..Y            __\e[1;90m|  |  |\n");
+      strcat (tempStr, "\e[1;92m \\   /`.        __|  |\e[1;90m  |  |\n");
+      strcat (tempStr, "\e[1;92m  \\.    \\    __|  |  |\e[1;90m  |  |\n");
+      strcat (tempStr, "\e[1;92m   \"\"\"--'   |  |  |  |\e[1;90m  |  |");
+    }
+  else if (power >= -80)
+    {
+      strcat (tempStr, "\e[1;90m                         __\n");
+      strcat (tempStr, "\e[1;90m                      __|  |\n");
+      strcat (tempStr, "\e[1;92m ,--..Y\e[1;90m            __|  |  |\n");
+      strcat (tempStr, "\e[1;92m \\   /`.        __\e[1;90m|  |  |  |\n");
+      strcat (tempStr, "\e[1;92m  \\.    \\    __|  |\e[1;90m  |  |  |\n");
+      strcat (tempStr, "\e[1;92m   \"\"\"--'   |  |  |\e[1;90m  |  |  |");
+    }
+  else
+    {
+      strcat (tempStr, "\e[1;90m                         __\n");
+      strcat (tempStr, "\e[1;90m                      __|  |\n");
+      strcat (tempStr, "\e[1;92m ,--..Y\e[1;90m            __|  |  |\n");
+      strcat (tempStr, "\e[1;92m \\   /`.\e[1;90m        __|  |  |  |\n");
+      strcat (tempStr, "\e[1;92m  \\.    \\    __\e[1;90m|  |  |  |  |\n");
+      strcat (tempStr, "\e[1;92m   \"\"\"--'   |  |\e[1;90m  |  |  |  |");
+    }
+  strcat (tempStr, "\e[0m\n");
+  strcat (buf, tempStr);
 }
 
 
@@ -110,27 +119,28 @@ int main (int argc, char **argv)
   signal (SIGQUIT, handleSignal);
   if (argc != 2)
     {
-      fprintf (stderr, "\e[1;31mUsage : wpower [WIRELESS INTERFACE]\n\e[0m");
+      fprintf (stderr, "\e[1;91mUsage : wpower [WIRELESS INTERFACE]\n\e[0m");
       exit (1);
     }
-  const char logo[] = "\e[1;34m\n __      ___     ___ _   ___                      __  __     _\n"
+  const char logo[] = "\e[1;94m\n __      ___     ___ _   ___                      __  __     _\n"
   " \\ \\    / (_)___| __(_) | _ \\_____ __ _____ _ _  |  \\/  |___| |_ ___ _ _\n"
   "  \\ \\/\\/ /| |___| _|| | |  _/ _ \\ V  V / -_) '_| | |\\/| / -_)  _/ -_) '_|\n"
   "   \\_/\\_/ |_|   |_| |_| |_| \\___/\\_/\\_/\\___|_|   |_|  |_\\___|\\__\\___|_|\n\n\e[0m";
   while (1)
     {
-      char outBuf[600];    // Output buffer
-      char infoBuf[300];    // Info buffer
+      char outBuf[900];    // Output buffer
+      char infoBuf[600];    // Info buffer
       char tempStr[50] = "";
-      sprintf (tempStr, "\e[1;33m Interface : %s\n\e[0m", argv[1]);
+      sprintf (tempStr, "\e[1;93m Interface : %s\n\e[0m", argv[1]);
       strcpy (infoBuf, tempStr);    // Store interface to buffer
       printEssid (argv[1], infoBuf);
       printRssi (argv[1], infoBuf);
       checkNic (argv[1]);    // Check if NIC is still up
-      printf ("\033c");    // Clear screen
       strcpy (outBuf, logo);
+      system ("clear");    // Clear screen
       printf (strcat (outBuf, infoBuf));    // Print everything
       sleep (1);    // Refresh delay
+      counter++;
     }
   return 0;
 }
